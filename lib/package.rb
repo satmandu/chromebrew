a3300062fc90ba256e21128996ed355826149be4
satmandu <satadru@umich.edu>
2020-11-02 12:19:41 -0500

    try something else

--- lib/package.rb
+++ lib/package.rb
@@ -115,7 +115,8 @@ class Package
     # See lib/const.rb for more details
 
     if @in_build == true
-      CREW_PREFIX = '/usrlocal'
+      CREW_PREFIX_SAVE = CREW_PREFIX
+      CREW_PREFIX.replace = '/usrlocal'
       nproc = ''
       nproc_opt =  ''
       args.each do |arg|
@@ -149,5 +150,6 @@ class Package
     Kernel.system(*args)
     exitstatus = $?.exitstatus
     raise InstallError.new("`#{args.join(" ")}` exited with #{exitstatus}") unless exitstatus == 0
+    CREW_PREFIX.replace = CREW_PREFIX_SAVE
   end
 end
