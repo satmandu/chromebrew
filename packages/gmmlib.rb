f05c2a9a2b6dc0837c5acd80dd3eb0078cbd116d
satmandu <satadru@gmail.com>
2021-03-10 16:31:33 -0500

    Update gmmlib.rb

--- packages/gmmlib.rb
+++ packages/gmmlib.rb
@@ -6,21 +6,21 @@ class Gmmlib < Package
   @_ver = '20.4.1'
   version @_ver
   compatibility 'i686 x86_64'
-  case ARCH
-  when 'i686', 'x86_64'
+  if ARCH == 'i686' || 'x86_64'
     source_url "https://github.com/intel/gmmlib/archive/intel-gmmlib-#{@_ver}.tar.gz"
     source_sha256 'd54d547f9f9e74196dead6a338923039ea10c859f1f693f33f10be1562b81d6d'
     depends_on 'libva'
-  end
 
-  binary_url({
-    i686: 'https://dl.bintray.com/chromebrew/chromebrew/gmmlib-20.4.1-chromeos-i686.tar.xz',
-  x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gmmlib-20.4.1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    i686: '216f1914cffe386625f78bc930046afa0d04f5254c555bd17fb433e70d161f17',
-  x86_64: '7d846916ab47e45d4d85dc41c08c718d0c4e4e774769886f8b452af8b041de92'
-  })
+    binary_url({
+      i686: 'https://dl.bintray.com/chromebrew/chromebrew/gmmlib-20.4.1-chromeos-i686.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gmmlib-20.4.1-chromeos-x86_64.tar.xz'
+    })
+    binary_sha256({
+      i686: '216f1914cffe386625f78bc930046afa0d04f5254c555bd17fb433e70d161f17',
+    x86_64: '7d846916ab47e45d4d85dc41c08c718d0c4e4e774769886f8b452af8b041de92'
+    })
+
+  end
 
   def self.patch
     system "find . -type f -exec sed -e 's,LD_LIBRARY_PATH=,LD_LIBRARY_PATH=#{CREW_LIB_PREFIX}:,g' \
