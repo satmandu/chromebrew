880921a01dd178ac2843d4c31225ffbd05091d80
Satadru Pramanik <satadru@gmail.com>
2024-05-17 10:49:55 -0400

    debug
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- commands/remove.rb
+++ commands/remove.rb
@@ -59,7 +59,7 @@ class Command
     # Remove the package from the list of installed packages in device.json.
     puts "Removing package #{pkg.name} from device.json".yellow if verbose
     pkg_name = pkg.name.to_s
-    device_json[:installed_packages].delete_if { |elem| elem[:name] == pkg[:pkg_name] }
+    device_json[:installed_packages].delete_if { |elem| elem[:name] == pkg_name }
 
     # Update device.json with our changes.
     save_json(device_json)
