f8545e2dd5706d208d9ea1f78d3e4c4f4243b39e
Satadru Pramanik <satadru@gmail.com>
2024-05-17 09:11:53 -0400

    debug
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/test.rb
+++ lib/test.rb
@@ -4,10 +4,10 @@ require_relative 'const'
 require_relative 'crewlog'
 
 def load_json(json_object)
-CREW_CONFIG_PATH      = File.join('/usr/local', 'etc/crew')
+  crew_config_path = '/usr/local/etc/crew'
 
   # load_json(): (re)load device.json
-  json_path = File.join(CREW_CONFIG_PATH, 'device.json')
+  json_path = File.join(crew_config_path, 'device.json')
   json_object = JSON.load_file(json_path, symbolize_names: true)
 
   # symbolize also values
