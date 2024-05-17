a78adc13f3ea34ec12fa3d69613a62b050837e80
Satadru Pramanik <satadru@gmail.com>
2024-05-17 09:35:34 -0400

    debug
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- commands/remove.rb
+++ commands/remove.rb
@@ -28,7 +28,7 @@ class Command
     unless pkg.is_fake?
       Dir.chdir CREW_CONFIG_PATH do
         # Remove all files installed by the package.
-        if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.filelist")
+        if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.filelist"))
           File.foreach(File.join(CREW_META_PATH, "#{pkg.name}.filelist"), chomp: true) do |line|
             next unless line.start_with?(CREW_PREFIX)
             if system("grep --exclude #{pkg.name}.filelist -Fxq '#{line}' ./meta/*.filelist")
@@ -41,7 +41,7 @@ class Command
         end
 
         # Remove all directories installed by the package.
-        if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.directorylist")
+        if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.directorylist"))
           File.foreach(File.join(CREW_META_PATH, "#{pkg.name}.directorylist"), chomp: true) do |line|
             next unless Dir.exist?(line) && Dir.empty?(line) && line.include?(CREW_PREFIX)
             puts "Removing directory #{line}".yellow if verbose
@@ -50,8 +50,8 @@ class Command
         end
 
         # Remove the file and directory lists.
-        FileUtils.remove_file File.join(CREW_META_PATH, "#{pkg.name}.filelist") if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.filelist")
-        FileUtils.remove_file File.join(CREW_META_PATH, "#{pkg.name}.directorylist") if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.directorylist")
+        FileUtils.remove_file File.join(CREW_META_PATH, "#{pkg.name}.filelist") if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.filelist"))
+        FileUtils.remove_file File.join(CREW_META_PATH, "#{pkg.name}.directorylist") if File.file?(File.join(CREW_META_PATH, "#{pkg.name}.directorylist"))
       end
     end
 
